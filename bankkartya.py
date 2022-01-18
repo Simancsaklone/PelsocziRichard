e1af6899db0ddb316527c5ef2ec2f76fd4c05094
pelsoczirichards <pelsoczirichards@EOTVOS2.LOCAL>
2022-01-18 12:19:00 +0100

    bakkártya

---
+++ bankkartya.py
@@ -0,0 +1,12 @@
+import random
+import os
+
+def bankKártya();
+    bankszam='0101-0101-0101-0101'
+    szamok='1','2','3','4','5','6','7','8','9'
+    szamlista=list(szamok)
+    if  x =0,1
+    else
+return
+
+bszam
